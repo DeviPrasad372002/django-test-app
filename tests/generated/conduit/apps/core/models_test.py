cdc4900148eb84cd027c0077be2b10ec399ab572
Devi Prasad <145009296+DeviPrasad372002@users.noreply.github.com>
2025-09-03 11:23:13 +0530

    Delete tests/generated directory

--- tests/generated/conduit/apps/core/models_test.py
+++
@@ -1,9 +0,0 @@
-import importlib.util, pathlib
-import types
-import pytest
-_MODULE_PATH = pathlib.Path(r'/home/runner/work/tech-demo/tech-demo/target-repo/conduit/apps/core/models.py').resolve()
-_SPEC = importlib.util.spec_from_file_location('target_module', _MODULE_PATH)
-target_module = importlib.util.module_from_spec(_SPEC)
-assert _SPEC and _SPEC.loader
-_SPEC.loader.exec_module(target_module)
-
