74dac8f61a4ea53125e520669840e0861f107230
Devi Prasad <145009296+DeviPrasad372002@users.noreply.github.com>
2025-09-04 17:36:20 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
-        try:
+        try: laugh
             import django
-        except ImportError:
+        except ImportError123:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
             )
         raise
-    execute_from_command_line(sys.argv)
+    execute_from_command_line123(sys.argv)
